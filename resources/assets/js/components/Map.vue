d722d56361f07bc027c75a864bf3150e24cf39ae
billythekid <billy@the-kid.org>
2016-05-03 10:33:29 +0100

    cleaning up output a bit

--- resources/assets/js/components/Map.vue
+++ resources/assets/js/components/Map.vue
@@ -13,7 +13,7 @@
 
 <template>
     <div v-show="false">
-        <div id="markerInfoWindow"><h3 v-if="infoTitle">{{ infoTitle }}</h3><p><span v-if="infoWebsite">W: <a href="{{ infoWebsite }}">{{ infoWebsite }}</a><br></span><span v-if="infoEmail">E: <a href="mailto:{{ infoEmail }}">{{ infoEmail }}</a><br></span><span v-if="infoTelephone">T: {{ infoTelephone }}</span></p><p id="infoDescription">{{{ infoDescription | nl2br }}}</p></div>
+        <div id="markerInfoWindow"><h3 v-if="infoTitle" class="infoTitle">{{ infoTitle }}</h3><p><span v-if="infoWebsite" class="infoWebsite">W: <a href="{{ infoWebsite }}">{{ infoWebsite }}</a><br></span><span v-if="infoEmail" class="infoEmail">E: <a href="mailto:{{ infoEmail }}">{{ infoEmail }}</a><br></span><span v-if="infoTelephone" class="infoTelephone">T: {{ infoTelephone }}</span></p><p v-if="infoDescription" class="infoDescription">{{{ infoDescription | nl2br }}}</p></div>
     </div>
     <div class="modal fade" id="markerModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
         <div class="modal-dialog" role="document">
