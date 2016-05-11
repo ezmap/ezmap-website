c6d7b8321088148bc6f067498fdbcce428b9a2ae
billythekid <billy@the-kid.org>
2016-05-11 22:41:10 +0100

    title

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'EZ Map')</title>
+    <title>@yield('title', 'EZ Map - Google Maps Made Easy')</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
     <link rel="icon" type="image/png" href="{{ url('favicon.png') }}" />
     <link href='https://fonts.googleapis.com/css?family=Kanit|Cutive Mono' rel='stylesheet' type='text/css'>
