f20c865d9fbb2239cdb2cb5c4c035118aaac185b
billythekid <billy@the-kid.org>
2016-05-11 16:29:46 +0100

    styling

--- resources/views/map/index.blade.php
+++ resources/views/map/index.blade.php
@@ -12,19 +12,19 @@
             <hr>
             <h2>Your Saved Maps</h2>
             @foreach(Auth::user()->maps as $map)
-                <div class="col-xs-8">
+                <div class="col-xs-10">
                     <div class="form-group">
                         <a class="btn btn-info form-control" href="{{ route('map.edit', $map) }}"><i class="fa fa-map-o"></i> {{ $map->title }}
                         </a>
                     </div>
                 </div>
-                <div class="col-xs-4">
+                <div class="col-xs-2">
                     <form action="{{ route('map.destroy', $map) }}" method="POST">
                         {{ method_field('DELETE') }}
                         {{ csrf_field() }}
                         <div class="form-group">
                             <button class="btn btn-danger form-control">
-                                <i class="fa fa-trash"></i> Delete Map
+                                <i class="fa fa-trash fa-fw"></i>
                             </button>
                         </div>
                     </form>
