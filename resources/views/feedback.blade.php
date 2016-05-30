edda7900f217011e9024fb44acf7e152f081a804
billythekid <billy@the-kid.org>
2016-05-30 22:47:08 +0100

    feedback form help text

--- resources/views/feedback.blade.php
+++ resources/views/feedback.blade.php
@@ -5,6 +5,9 @@
 
         <h3>Please give any feedback on this tool here.</h3>
         <hr>
+        <p>Maybe you would like a particular <a href="http://snazzymaps.com">Snazzy Maps</a> theme installed, or something's being
+            problematic with markers. Whatever your issue, please please contact me and I'll do what I can to fix you
+            right up.</p>
         @include('partials.feedbackForm')
     </div>
 @endsection
