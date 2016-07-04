4604ee63452e5fcb8e9cdf7305514aeca1abee7b
billythekid <billy@the-kid.org>
2016-07-04 20:48:31 +0100

    move google maps code to pull in via js

--- resources/views/map/show.blade.php
+++ resources/views/map/show.blade.php
@@ -1,24 +1,23 @@
-{!! file_get_contents("https://maps.googleapis.com/maps/api/js?key=" . $map->apiKey) !!}
+head = document.head || document.getElementsByTagName('head')[0],
+
+gmapscript = document.createElement('script');
+gmapscript.src = "https://maps.googleapis.com/maps/api/js?key={{ $map->apiKey }}"
+head.appendChild(gmapscript);
+
+var css = '#{{ $map->mapContainer }}{min-height: 150px;min-width: 150px;width: {{ $map->responsiveMap ? "100%" : "{$map->width}px"}};height: {{ $map->height }}px;}',
+style = document.createElement('style');
+style.type = 'text/css';
+if (style.styleSheet){
+    style.styleSheet.cssText = css;
+} else {
+    style.appendChild(document.createTextNode(css));
+}
+head.appendChild(style);
+
 mapContainer = document.createElement('div');
 mapContainer.id = '{{ $map->mapContainer }}';
 document.body.insertBefore(mapContainer, document.getElementById('ez-map-embed-script'));
 
-var css = ' #{{ $map->mapContainer }}{'+
-    'min-height:  150px;'+
-    'min-width:   150px;'+
-    'width:      {{ $map->responsiveMap ? "100%" : "{$map->width}px"}};'+
-    'height:     {{ $map->height }}px;'+
-'}',
-head = document.head || document.getElementsByTagName('head')[0],
-style = document.createElement('style');
-
-style.type = 'text/css';
-if (style.styleSheet){
-style.styleSheet.cssText = css;
-} else {
-style.appendChild(document.createTextNode(css));
-}
-
-head.appendChild(style);
-
-{!! $map->code() !!}
+gmapscript.onload = function(){
+    {!! $map->code() !!}
+};
