f6004247ffc69e87b335da9dfbffad89ca28a0c8
billythekid <billy@the-kid.org>
2016-07-05 13:25:29 +0100

    sake!

--- resources/views/partials/textareaembedcode.blade.php
+++ resources/views/partials/textareaembedcode.blade.php
@@ -1,3 +1,3 @@
 &lt;!-- Google map code from EZ Map - https://ezmap.co -->
-&lt;script src="http://ezmap.dev/map/{{ $map->id }}" id="ez-map-embed-script">&lt;/script>
+&lt;script src="http://ezmap.co/map/{{ $map->id }}" id="ez-map-embed-script">&lt;/script>
 &lt;!-- End of EZ Map code - https://ezmap.co -->
