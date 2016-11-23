04f343e33435f3e4dcacbba0859ac10d3e809f54
Billy Fagan <billy@fortytwo.studio>
2016-11-23 19:37:54 +0000

    just get it to work, worry about warnings at a later time

--- resources/views/twomapsautoupdate.blade.php
+++ resources/views/twomapsautoupdate.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Two Auto-Update maps on a page</title>
 </head>
 <body>
 <!-- Google map code from EZ Map - https://ezmap.co -->
