493a28716c2043fd96a728a5cfdf4e1a8e22ed80
Billy Fagan <billy@fortytwo.studio>
2016-11-23 19:18:15 +0000

    randomly stabbing js in teh dark

--- resources/views/map/show.blade.php
+++ resources/views/map/show.blade.php
@@ -1,7 +1,7 @@
 (function(){
 var head = document.head || document.getElementsByTagName('head')[0];
-
-if (typeof window.gmapscript === undefined)
+console.log(typeof document.getElementById("ezmap-gmap-script"));
+if (typeof document.getElementById("ezmap-gmap-script") === undefined)
 {
 var gmapscript = document.createElement('script');
 gmapscript.id = "ezmap-gmap-script";
