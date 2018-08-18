38c222528b4a63322f4dceccd28c5b1a72f94fd7
Billy Fagan <billy@the-kid.org>
2018-08-19 00:49:02 +0100

    little bit of validation

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,14 +11,29 @@ use App\Http\Requests;
 class ApiController extends Controller
 {
 
+  private function checkApiAvailable($email, $apiKey)
+  {
+    $user = User::where(['email' => $email])->firstOrFail();
+
+    if ($user->apikey === "" || $apiKey !== $user->apikey)
+    {
+      abort(404);
+    }
+  }
+
   public function getMaps($email, $apiKey)
   {
+    $this->checkApiAvailable($email, $apiKey);
+
     $user = User::where(['email' => $email, 'apikey' => $apiKey])->firstOrFail();
 
     return $user->maps;
   }
 
-  public function getMapCode($email, $apiKey, Map $map){
+  public function getMapCode( $email, $apiKey, Map $map)
+  {
+    $this->checkApiAvailable($email, $apiKey);
+
     $user = User::where(['email' => $email, 'apikey' => $apiKey])->firstOrFail();
 
     if ($map->user->id === $user->id)
@@ -27,9 +42,13 @@ class ApiController extends Controller
       {
         return view('api.embedcode', compact('map'));
       }
+
       return view('api.mapcode', compact('map'));
     }
 
+    return "";
+
   }
+
 
 }
