a162bc5670fe28a54372f00908cf2091c8f6b2f6
Billy Fagan <billy@the-kid.org>
2018-08-18 20:28:50 +0100

    error feedback on login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,28 +10,28 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {!! csrf_field() !!}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <div class="form-group">
                             <label class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
                                 <input type="email" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
+                                    <span class="help-block alert alert-danger">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <div class="form-group">
                             <label class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
                                 <input type="password" class="form-control" name="password">
 
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
+                                    <span class="help-block alert alert-danger">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
