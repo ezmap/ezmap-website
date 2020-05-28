49dfb7d4fe4c3602337d95623d9ec6f2a7484f5b
Billy Fagan <billy@the-kid.org>
2020-05-28 12:18:24 +0100

    record last login time and login IP address

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,76 +2,101 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\User;
-use Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
+  /*
+  |--------------------------------------------------------------------------
+  | Registration & Login Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles the registration of new users, as well as the
+  | authentication of existing users. By default, this controller uses
+  | a simple trait to add these behaviors. Why don't you explore it?
+  |
+  */
 
-    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+  use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
-    protected $redirectAfterLogout = 'login';
+  /**
+   * Where to redirect users after login / registration.
+   *
+   * @var string
+   */
+  protected $redirectTo = '/';
+  protected $redirectAfterLogout = 'login';
 
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
-    }
+  /**
+   * Create a new authentication controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+  }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
-            'g-recaptcha-response' => 'required|recaptcha',
-        ],
-            [
-                'g-recaptcha-response.required' => "We need to know you're not a robot"
-            ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+  /**
+   * Get a validator for an incoming registration request.
+   *
+   * @param array $data
+   * @return \Illuminate\Contracts\Validation\Validator
+   */
+  protected function validator(array $data)
+  {
+    return Validator::make($data, [
+        'name'                 => 'required|max:255',
+        'email'                => 'required|email|max:255|unique:users',
+        'password'             => 'required|min:6|confirmed',
+        'g-recaptcha-response' => 'required|recaptcha',
+    ],
+        [
+            'g-recaptcha-response.required' => "We need to know you're not a robot",
         ]);
-    }
+  }
+
+  /**
+   * Create a new user instance after a valid registration.
+   *
+   * @param array $data
+   * @return User
+   */
+  protected function create(array $data)
+  {
+    return User::create([
+        'name'     => $data['name'],
+        'email'    => $data['email'],
+        'password' => bcrypt($data['password']),
+    ]);
+  }
+
+  /**
+   * @param Request $request
+   * @param         $user
+   */
+  protected function authenticated(Request $request, $user)
+  {
+    $this->recordLogin($user, $request);
+
+    return redirect()->intended($this->redirectPath());
+  }
+
+  /**
+   * @param         $user
+   * @param Request $request
+   */
+  protected function recordLogin($user, Request $request): void
+  {
+    $user->update([
+        'last_login_at' => DB::raw('now()'),
+        'last_login_ip' => $request->getClientIp()
+    ]);
+  }
 }
