e36b71910fc9f6d2f0f17657b5cae026ba2b2459
billythekid <billythekid@github.com>
2022-03-18 20:48:42 +0000

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\GeneralController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MapController;
+use App\Http\Controllers\SnazzyMapsController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,11 +21,11 @@ use Illuminate\Support\Facades\Auth;
 */
 
 
-Route::get('/', "GeneralController@index")->middleware(['guest', 'pjax'])->name('index');
+Route::get('/', [GeneralController::class, "index"])->middleware(['guest', 'pjax'])->name('index');
 Route::get('feedback', function () {
   return view('feedback');
 });
-Route::post('feedback', 'GeneralController@feedback')->name('feedback');
+Route::post('feedback', [GeneralController::class, "feedback"])->name('feedback');
 
 Route:: get('test', function () {
   return view('test');
@@ -28,28 +34,28 @@ Route:: get('test', function () {
 //Route::auth();
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/logout', 'Auth\LoginController@logout');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/logout', [LoginController::class, 'logout']);
 
-Route::get('admin', 'AdminController@index');
-Route::get('stealth/{userid}', "AdminController@stealth")->name('stealth');
-Route::get('unstealth', "AdminController@unstealth")->name('unstealth');
-Route::post('admin', 'SnazzyMapsController@populateThemes')->name('populateThemes')->middleware('admin');
+Route::get('admin', [AdminController::class, 'index']);
+Route::get('stealth/{userid}', [AdminController::class, "stealth"])->name('stealth');
+Route::get('unstealth', [AdminController::class, "unstealth"])->name('unstealth');
+Route::post('admin', [SnazzyMapsController::class, 'populateThemes'])->name('populateThemes')->middleware('admin');
 
-Route::post('addNewIcon', 'HomeController@addNewIcon')->name('addNewIcon');
-Route::post('deleteIcon', 'HomeController@deleteIcon')->name('deleteIcon');
-Route::post('geocodeAddress', 'GeneralController@geocodeAddress')->name('geocode');
+Route::post('addNewIcon', [HomeController::class, 'addNewIcon'])->name('addNewIcon');
+Route::post('deleteIcon', [HomeController::class, 'deleteIcon'])->name('deleteIcon');
+Route::post('geocodeAddress', [GeneralController::class, 'geocodeAddress'])->name('geocode');
 
-Route::post('addMarkerIcon', 'AdminController@addMarkerIcon')->name('addMarkerIcon');
+Route::post('addMarkerIcon', [AdminController::class, 'addMarkerIcon'])->name('addMarkerIcon');
 
-Route::get('AZPopulate', 'AdminController@AZPopulate')->name('AZPopulate');
+Route::get('AZPopulate', [AdminController::class, 'AZPopulate'])->name('AZPopulate');
 
-Route::resource('map', 'MapController');
-Route::post('map/undelete/{id}', 'MapController@undelete')->name('map.undelete');
-Route::get('map/image/{map}', 'MapController@image')->name('map.image');
-Route::get('map/image/download/{map}', 'MapController@download')->name('map.download');
+Route::resource('map', MapController::class);
+Route::post('map/undelete/{id}', [MapController::class, 'undelete'])->name('map.undelete');
+Route::get('map/image/{map}', [MapController::class, 'image'])->name('map.image');
+Route::get('map/image/download/{map}', [MapController::class, 'download'])->name('map.download');
 
-Route::get('lang/{lang}', 'GeneralController@changeLanguage')->name('lang');
+Route::get('lang/{lang}', [GeneralController::class, 'changeLanguage'])->name('lang');
 
 Route::get('help', function () {
   return view('help.index');
@@ -59,8 +65,8 @@ Route::get('twomapautoupdatetest', function () {
   return view('twomapsautoupdate');
 });
 
-Route::post('renewapikey', 'HomeController@renewApiKey')->name('renewapikey');
+Route::post('renewapikey', [HomeController::class, 'renewApiKey'])->name('renewapikey');
 
-Route::get('api', function(){
+Route::get('api', function () {
   return view('api.about');
 })->name('api');
