34cf11c0c0dd513c730990015541126676c86802
billythekid <billythekid@github.com>
2023-03-30 10:13:29 +0100

    remove old analytics code

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,7 +21,6 @@
     }
 
     gtag('set', 'allow_google_signals', false);
-    gtag('set', 'allow_ad_personalization_signals', false);
     gtag('js', new Date());
     gtag('config', 'G-5ESRGRL3QX');
   </script>
@@ -120,20 +119,5 @@
       }
     </script>
 
-    <script>
-      (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-          (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-          m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-      })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-      ga('create', 'UA-77351189-1', 'auto');
-      ga('send', 'pageview');
-    </script>
   </body>
 </html>
